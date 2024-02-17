d7566319093fcc4212eea6c9194023e05c2b98a6
Nishant Srivastava <nishantsr@infinite.com>
2024-02-17 09:01:45 -0500

    main-file

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,16 @@
- 
+module "rg" {
+  source = "./modules/resource_group"
+  name   = "rg-web"
+  location = "East US" // Example location, customize as needed
+}
+
+
+module "network" {
+  source             = "./modules/network"
+  resource_group_name = module.rg.name
+}
+
+module "vm_linux_app" {
+  source                  = "./modules/vm/linux_vm"
+  resource_group_name     = module.rg_app.name
+}
