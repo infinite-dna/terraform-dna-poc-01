bdf601d92ea3902e1eca2baa182a711a28586ff8
Nishant Srivastava <nishantsr@infinite.com>
2024-02-29 06:48:08 -0500

    automation scripts

--- automation-scripts/run-pre-install-automate.ps1
+++ automation-scripts/run-pre-install-automate.ps1
@@ -19,8 +19,18 @@ $scripts = @(
     "oracle-step2-preReq.ps1"
 )
 
+# Start transcript logging
+$timestamp = Get-Date -Format "yyyyMMddHHmmss"
+$logFile = Join-Path -Path $scriptDir -ChildPath "script_execution_$timestamp.log"
+Start-Transcript -Path $logFile
+
 # Run each script one by one
 foreach ($script in $scripts) {
     $scriptPath = Join-Path -Path $scriptDir -ChildPath $script
+    Write-Host "Executing $scriptPath"
     Start-Process powershell.exe -ArgumentList "-ExecutionPolicy Bypass -File `"$scriptPath`"" -Wait
+    Write-Host "Completed $scriptPath"
 }
+
+# Stop transcript logging
+Stop-Transcript
