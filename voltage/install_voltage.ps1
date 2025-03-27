40dd9546e84add8fd3326f6a79a4e2f31ea881a9
infinite-dna <148960372+infinite-dna@users.noreply.github.com>
2025-03-27 17:00:09 +0530

    Update install_voltage.ps1

--- voltage/install_voltage.ps1
+++ voltage/install_voltage.ps1
@@ -1,8 +1,10 @@
+$setupPath = "C:\Path\To\Setup.exe"  # Replace with the actual full path
+
 $arguments = '/s /v"/1*V \"C:\Program Files\Open Solutions Installation Information\DNA4\Log_1.txt\" ' +
 'ENVNAME=\"PROD, BKTEST001D\" DMCONNECTSTRING=\"Data Source=10.219.149.7:1521/BKTest001D; ' +
 'User Id=DEPLOYMGR;Password=deploymgr\" CLIENTONLY=FALSE /Quiet"'
 
-$process = Start-Process -FilePath "Setup.exe" -ArgumentList $arguments -NoNewWindow -PassThru
+$process = Start-Process -FilePath $setupPath -ArgumentList $arguments -NoNewWindow -PassThru
 
 $process.WaitForExit()
 
